c702978817c88accfb5b063328d0bb693a57cf46
amitesh kumar <amiteshptn@gmail.com>
2022-08-26 00:14:24 +0530

    added most of the functionality

--- Todo List/script.js
+++ Todo List/script.js
@@ -41,6 +41,9 @@ modeEl.addEventListener('click',()=>{
 //dark mode till here
 
 //Adding tasks
+let count = 0;
+let countEL = document.getElementById('countLeft');
+
 const addBtnEl =  document.getElementById('addBtn');
 const newTaskContainer = document.querySelector('.tasks');
 const taskConatiner = document.getElementById('todo-input');
@@ -52,6 +55,9 @@ addBtnEl.addEventListener('click', () =>{
         alert("Please enter a task first");
     }
     else {
+        count++
+    countEL.innerText =count  + ' ' ;
+
         let mainConatiner = document.createElement('div');
         mainConatiner.classList.add('adding-container');
         newTaskContainer.appendChild(mainConatiner);
@@ -69,9 +75,7 @@ addBtnEl.addEventListener('click', () =>{
 
         taskText.textContent = `${taskConatiner.value}`;
 
-
         mainConatiner.appendChild(taskText);
-
 
         taskConatiner.value = "";
 
@@ -81,22 +85,99 @@ addBtnEl.addEventListener('click', () =>{
         checkBtn.addEventListener('click', () => {
             if (strike === 'disable') {
                 checkBtn.parentElement.classList.add('strike-through');
-                console.log(checkBtn)
+
+                count--;
+                countEL.innerText =count  + ' ' ;
+
                 strike = 'enable';
                 taskText.style.color = "var(--placeholder-text-color)";
                 checkBtn.innerHTML = `<img src="./images/icon-check.svg" alt="">`
                 checkBtn.style.background ='linear-gradient(135deg,var(--gradient-1),var(--gradient-2))';
+
+
             } else {
+
                 checkBtn.parentElement.classList.remove('strike-through');
                 strike = 'disable';
                 taskText.style.color = "var(--pannel-active-text)";
                 checkBtn.innerHTML =``;
                 checkBtn.style.background =`none`;
+
+                count++;
+                countEL.innerText =count  + ' ' ;
             }
         })
 
     //    delete btn here
+        taskText.addEventListener('click',()=>{
 
+            if(taskText.parentElement.classList.contains('strike-through')) {
+                taskText.parentElement.remove();
+                count--;
+                countEL.innerText =count  + ' ' ;
+            }
+        })
+
+    //    delete all btn
+
+        let deleteAll = document.querySelector('.complete-state');
+
+            deleteAll.addEventListener('click',()=>{
+
+                   let completeTask = document.getElementsByClassName('strike-through');
+
+                   for (let i=0;i<completeTask.length;i++){
+                       completeTask[i].remove();
+                       count--;
+                       countEL.innerText =count  + ' ' ;
+                   }
+            })
+
+    //three states
+
+            //Active state
+            let activeEl = document.querySelector('.Active');
+
+
+            activeEl.addEventListener('click',()=>{
+                let completeTask = document.getElementsByClassName('strike-through');
+
+                for (let i=0;i<completeTask.length;i++){
+                    completeTask[i].style.display='none';
+                }
+
+
+            })
+
+    //  All state
+        let allEl = document.querySelector('.all');
+
+
+        allEl.addEventListener('click',()=>{
+            let completeTask = document.getElementsByClassName('adding-container');
+
+            for (let i=0;i<completeTask.length;i++){
+                completeTask[i].style.display='flex';
+            }
+
+
+        })
+
+    //    complete state
+        let completeEl = document.querySelector('.completed');
+
+        completeEl.addEventListener('click',()=>{
+            let completeTask = document.getElementsByClassName('adding-container');
+
+
+            for (let i=0;i<completeTask.length;i++) {
+                if (completeTask[i].classList.contains('strike-through')) {
+                    completeTask[i].style.display = 'flex';
+                } else {
+                    completeTask[i].style.display = 'none';
+                }
+            }
+        })
 
 
     }
